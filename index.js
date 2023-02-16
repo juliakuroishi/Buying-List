3e29974736f81689a8dac45c753647662e846ade
Julia Kuroishi <juliabkuroishi@gmail.com>
2023-02-16 16:52:13 -0400

    enviando mas com correções ainda para fazer

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-alert('Hello, we are still working on this page :)');
+//alert('Hello, we are still working on this page :)');
+
+function adcItem(){
+    var item = document.getElementById('idinput').value
+    console.log(item)
+
+    var novoElem  = document.createElement('li');
+    texto = document.createTextNode(item);
+    novoElem.appendChild(texto);
+    console.log(novoElem);
+    const body = document.body;
+    body.appendChild(novoElem);
+};
+
+function deleteItem(){
+
+};
